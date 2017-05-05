1467b7af00cf35fbc46b2a979cc2a489f748297e
zengjinjie <zengjinjie@gmail.com>
2017-05-05 23:26:16 +0800

    Find that jpeg mode solves the delay effect and the "tolerance" parameter of "face_recognition.compare_faces" is better set in 0.4 rather than the official recommend 0.6.

---
+++ JpgRead.py
@@ -0,0 +1,41 @@
+import cv2
+import numpy as np
+from urllib import request
+import face_recognition
+import random
+import time
+
+boss_image = face_recognition.load_image_file("IMG_1778.JPG")
+boss_face_encoding = face_recognition.face_encodings(boss_image)[0]
+face_locaions = []
+face_encodings = []
+face_names = []
+k = 0
+
+while True:
+    with request.urlopen("http://192.168.2.112:8080/shot.jpg") as f:
+        data = f.read()
+        # print(f.status, f.reason)
+        # for k, v in f.getheaders():
+        #     print('%s: %s' % (k, v))
+        # print(data)
+        nparr = np.fromstring(data, np.uint8)
+        # print(nparr)
+        # print()
+        i = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+
+        face_locaions = face_recognition.face_locations(i)
+        face_encodings = face_recognition.face_encodings(i, face_locaions)
+
+        face_names = []
+        for face_encoding in face_encodings:
+            match = face_recognition.compare_faces([boss_face_encoding], face_encoding, tolerance=0.4)
+            name = "Unknown"
+
+            if match[0]:
+                name = "Boss"
+                print("Boss is coming")
+
+        # cv2.imshow('JPEG', i)
+        k += 1
+        print(k)
